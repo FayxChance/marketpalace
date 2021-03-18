346341874dfa61e6a47d301cee422984a754d068
FayxChance <fayxchancetom@outlook.com>
2021-03-18 13:30:14 +0100

    Modify css

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,48 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from "../product";
-import { PRODUCTS } from "../mock-products";
-import { RequestService } from "../request/request.service";
+import { Product } from '../product';
+import { PRODUCTS } from '../mock-products';
+import { RequestService } from '../request/request.service';
 import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  selectedProduct ?: Product;
-  distance ?: number;
+  selectedProduct?: Product;
+  distance?: number;
   products = PRODUCTS;
-  deliveryPrice ?: number;
-  totalPrice ?: number;
-  onSelect(product : Product): void {
+  deliveryPrice?: number;
+  totalPrice?: number;
+
+  onSelect(product: Product): void {
     this.selectedProduct = product;
     this.distance = undefined;
     this.deliveryPrice = undefined;
     this.totalPrice = undefined;
   }
 
-  deliveryPriceRequest(product : Product) :  void {
+  deliveryPriceRequest(product: Product): void {
     if (this.distance) {
-      var params = {"weight" : product.weight, "distance" : this.distance};
+      var params = { weight: product.weight, distance: this.distance };
       var value = this.requestService.get(params);
-      value.subscribe( (value) => {
-        console.log(value)
-        this.deliveryPrice = value["deliveryCost"];
-        if ( this.deliveryPrice)
+      value.subscribe((value) => {
+        this.deliveryPrice = value['deliveryCost'];
+        if (this.deliveryPrice)
           this.totalPrice = this.deliveryPrice + product.price;
       });
-        //this.deliveryPrice = value["deliveryCost"].valueOf());
-
     }
-
   }
 
-  constructor(private requestService: RequestService) { }
+  constructor(private requestService: RequestService) {}
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
